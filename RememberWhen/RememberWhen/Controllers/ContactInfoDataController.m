ea4ee8fe96e7b41d820023c720a4350fc95d78eb
David <david.aghassi@gmail.com>
2014-02-12 18:04:11 -0500

    Finished implementing the methods
    However, the data controller is throwing some weird errors I have to
    figure out regarding not seeing types and things. I don’t know why yet.

--- RememberWhen/RememberWhen/Controllers/ContactInfoDataController.m
+++ RememberWhen/RememberWhen/Controllers/ContactInfoDataController.m
@@ -10,12 +10,46 @@
 #import "ContactInfo.h"
 
 //Class extention with private initialize method
-@interface ContactInfoDataController : NSObject 
+@interface ContactInfoDataController ()
 
 -(void) initializeDefaultDataList;
 
 @end
 
 @implementation ContactInfoDataController
+
+-(void) initializeDefaultDataList{
+    NSMutableArray *contactList = [[NSMutableArray alloc]init];
+    
+    self.masterContactInfoList = contactList;
+}
+
+-(id)init{
+    if (self = [super init]) {
+        [self initializeDefaultDataList];
+        
+        return self;
+    }
+    
+    return nil;
+}
+
+
+//Methods implemented
+
+//Returns length of list
+-(NSUInteger)countOfList{
+    return [self.masterContactInfoList count];
+}
+
+//Returns the object at a given index
+-(ContactInfo *)objectInListAtIndex: (NSUInteger)index{
+    return [self.masterContactInfoList objectAtIndex:index];
+}
+
+//Adds an contact with info to the list
+-(void)addContactInfoWithInfo:(ContactInfo *)info{
+    [self.masterContactInfoList addObject:info];
+}
 
 @end
